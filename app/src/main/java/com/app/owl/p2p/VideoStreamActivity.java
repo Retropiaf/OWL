332420470875fde07cea8587d62a4f322d56d35e
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-01-11 19:57:10 -0800

    new VideoStreamActivity

---
+++ app/src/main/java/com/app/owl/p2p/VideoStreamActivity.java
@@ -0,0 +1,13 @@
+package com.app.owl.p2p;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
