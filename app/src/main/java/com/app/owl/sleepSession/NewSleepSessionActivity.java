617488e58e4896c0ad65111a88d196e25ea26716
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-01-22 20:06:55 -0800

    toggle betwen 'start monitoring' and 'start sleep session' on Newsleepsession activity

--- app/src/main/java/com/app/owl/sleepSession/NewSleepSessionActivity.java
+++ app/src/main/java/com/app/owl/sleepSession/NewSleepSessionActivity.java
@@ -1,9 +1,15 @@
 package com.app.owl.sleepSession;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -11,12 +17,16 @@ import android.widget.Spinner;
 import com.app.owl.R;
 import com.app.owl.sleepCircle.SleepCircle;
 import com.app.owl.sleepCircle.SleepCircleList;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,29 +34,51 @@ public class NewSleepSessionActivity extends AppCompatActivity {
 
     String TAG = "NewSleepSessionActivity";
     String selected = "";
+    String uid, ip, circleName, selection, circleMonitorIp;
+    InetAddress inetAddress;
     Spinner chooseCirclespinner;
     Button startSleepBtn, startMonitoringBtn;
     SleepCircleList sleepCircleList; // The adapter
     ArrayList<SleepCircle> list;
     DatabaseReference database;
+    ConnectivityManager connManager;
+    NetworkInfo myWifi;
+    WifiManager wifiManager;
+    Boolean monitorDevice;
+    SleepCircle circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_sleep_session);
 
+        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        myWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        /*
-        // Choose a Circle (or get circle from SleepCircle intent extra)
-        chooseCirclespinner = findViewById(R.id.chooseCirclespinner);
-        // Todo: fill in the spinner and add a method to get selected value
-        sleepCircleList = new SleepCircleList(NewSleepSessionActivity.this, list);
-        // Specify the layout to use when the list of choices appears
-        sleepCircleList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        chooseCirclespinner.setAdapter(sleepCircleList);
+        wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        */
+        if (!myWifi.isConnected()) {
+            wifiManager.setWifiEnabled(true);
+        }
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            try {
+                Log.d(TAG, "Before getting ip address");
+
+                inetAddress = InetAddress.getLocalHost();
+                ip = String.valueOf(inetAddress);
+
+                Log.d(TAG, "after getting ip address. ip = " + ip);
+            } catch (UnknownHostException e) {
+                System.out.println("I'm sorry. I don't know my own address. Connect to wifi, maybe?");
+            }
+
+        }
+
 
         chooseCirclespinner = findViewById(R.id.chooseCirclespinner);
 
@@ -55,12 +87,10 @@ public class NewSleepSessionActivity extends AppCompatActivity {
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Is better to use a List, because you don't know the size
-                // of the iterator returned by dataSnapshot.getChildren() to
-                // initialize the array
+
                 final List<String> sleepCircles = new ArrayList<String>();
 
-                for (DataSnapshot sleepCircleSnapshot: dataSnapshot.getChildren()) {
+                for (DataSnapshot sleepCircleSnapshot : dataSnapshot.getChildren()) {
                     String circleName = sleepCircleSnapshot.child("circleName").getValue(String.class);
                     sleepCircles.add(circleName);
                 }
@@ -78,12 +108,22 @@ public class NewSleepSessionActivity extends AppCompatActivity {
         });
 
 
-
-
-
-
-                // TODO: turn wifi on
-
+        startMonitoringBtn = findViewById(R.id.start_monitoring_btn);
+        startMonitoringBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected = String.valueOf(chooseCirclespinner.getSelectedItem());
+                if (!selected.equals("")) {
+                    Log.d(TAG, "Selection: " + selected);
+                } else {
+                    Log.d(TAG, "nothing is selected");
+                }
+                                        /*
+                                        Intent intent = new Intent(NewSleepSessionActivity.this, OnGoingMonitoringActivity.class);
+                                        startActivity(intent);
+                                        */
+            }
+        });
         startSleepBtn = findViewById(R.id.start_sleep_btn);
         startSleepBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,48 +132,91 @@ public class NewSleepSessionActivity extends AppCompatActivity {
                 selected = String.valueOf(chooseCirclespinner.getSelectedItem());
                 if (!selected.equals("")) {
                     Log.d(TAG, "Selection: " + selected);
-                }else{
+                } else {
                     Log.d(TAG, "nothing is selected");
                 }
-                /*
-                Intent intent = new Intent(NewSleepSessionActivity.this, OnGoingSleepSessionActivity.class);
-                startActivity(intent);
-                */
+                                        /*
+                                        Intent intent = new Intent(NewSleepSessionActivity.this, OnGoingSleepSessionActivity.class);
+                                        startActivity(intent);
+                                        */
             }
         });
 
-        startMonitoringBtn = findViewById(R.id.start_monitoring_btn);
-        startMonitoringBtn.setOnClickListener(new View.OnClickListener() {
+        startMonitoringBtn.setVisibility(View.INVISIBLE);
+        startSleepBtn.setVisibility(View.INVISIBLE);
+
+
+
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        chooseCirclespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                selected = String.valueOf(chooseCirclespinner.getSelectedItem());
-                if (!selected.equals("")) {
-                    Log.d(TAG, "Selection: " + selected);
-                }else{
-                    Log.d(TAG, "nothing is selected");
-                }
-                /*
-                Intent intent = new Intent(NewSleepSessionActivity.this, OnGoingMonitoringActivity.class);
-                startActivity(intent);
-                */
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                Log.d(TAG, "new item selected");
+                selection = String.valueOf(chooseCirclespinner.getSelectedItem());
+                database = FirebaseDatabase.getInstance().getReference("MainUsers/"+uid+"/circles/");
+                Log.d(TAG, "database: " + database);
+
+                startMonitoringBtn.setVisibility(View.INVISIBLE);
+                // set startSleepBtn visible
+                startSleepBtn.setVisibility(View.VISIBLE);
+
+                database.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        circle = dataSnapshot.getValue(SleepCircle.class);
+
+                        circleName = circle.getCircleName();
+                        circleMonitorIp = circle.getMonitorIp();
+
+                        if(selection.equals(circleName) && ip.equals(circleMonitorIp)){
+                            Log.d(TAG, "Selected circle device and current device are the same");
+                            startMonitoringBtn.setVisibility(View.VISIBLE);
+                            // set startSleepBtn invisible
+                            startSleepBtn.setVisibility(View.INVISIBLE);
+
+                        }
+                    }
+
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+                Log.d(TAG, "Nothing selected");
+                startMonitoringBtn.setVisibility(View.INVISIBLE);
+                startSleepBtn.setVisibility(View.INVISIBLE);
+
+            }
+
         });
 
 
-/*
-        if(true){
-            //set startMonitoringBtn visible
-            startMonitoringBtn.setVisibility(View.VISIBLE);
-            // set startSleepBtn invisible
-            startSleepBtn.setVisibility(View.INVISIBLE);
-        }else{
-            // JOIN SLEEP SESSION and or start monitpring on monitor device
-            //set startMonitoringBtn invisible
-            startMonitoringBtn.setVisibility(View.INVISIBLE);
-            // set startSleepBtn visible
-            startSleepBtn.setVisibility(View.INVISIBLE);
-        }
-        */
 
 
         //CANCEL
@@ -154,5 +237,68 @@ public class NewSleepSessionActivity extends AppCompatActivity {
         // Make the monitor the alert giver
 
         // new intent: open On Going sleep session activity // open On Going monitoring activity on the monitor
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*
+        monitorDevice = false;
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        database = FirebaseDatabase.getInstance().getReference().child("MainUser").child(uid).child("SleepCircles");
+        chooseCirclespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                try {
+                    inetAddress = InetAddress.getLocalHost();
+                    ip = String.valueOf(inetAddress);
+
+                    database.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                                SleepCircle circle = snapshot.getValue(SleepCircle .class);
+
+                                if(circle.getMonitorIp() == ip){
+                                    monitorDevice = true;
+                                }
+
+                            }
+                            if(monitorDevice){
+                                //set startMonitoringBtn visible
+                                startMonitoringBtn.setVisibility(View.VISIBLE);
+                                // set startSleepBtn invisible
+                                startSleepBtn.setVisibility(View.INVISIBLE);
+                            }else{
+                                // JOIN SLEEP SESSION and or start monitpring on monitor device
+                                //set startMonitoringBtn invisible
+                                startMonitoringBtn.setVisibility(View.INVISIBLE);
+                                // set startSleepBtn visible
+                                startSleepBtn.setVisibility(View.INVISIBLE);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                } catch (UnknownHostException e) {
+                    System.out.println("I'm sorry. I don't know my own address. Connect to wifi, maybe?");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
+        */
     }
 }
+
